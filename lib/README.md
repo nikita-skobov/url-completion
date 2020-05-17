f747f83d6789a5e93dd4ea194a8aaa177b500021
nikita-skobov <nskobov.websites@gmail.com>
2020-05-17 17:55:21 -0500

    adds library of helper functions for writing install scripts

---
+++ lib/README.md
@@ -0,0 +1,30 @@
+This is a library to help with writing install.sh files
+for the commands you wish to add.
+
+Simply source the `lib.sh` file in your `install.sh` scripts, and then you will have a series of helper functions to help you write install scripts easier. Take a look inside the `lib.sh` file to see what the functions are, they are fairly well documented.
+
+If you are writing a install script, every single install script should start with these lines:
+
+```sh
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+LIBPATH="$SCRIPTPATH/../../lib/lib.sh"
+if [[ -z $__COMPLETION_LIB_LOADED ]]; then source "$LIBPATH" ; fi
+default_startup_script="$HOME/.bashrc"
+startup_script="${1:-$default_startup_script}"
+
+source_url_completion_if_not_exist
+load_startup_script_if_not_exist "$startup_script"
+write_url_completion_to_file_if_not_exist "$startup_script"
+write_colon_removal_to_file_if_not_exist "$startup_script"
+```
+
+And after that you can write your code thats specific to your command. For example, for one of the git commands, I did:
+
+```sh
+if ! startup_script_has "eval __orig_\"\$(declare -f __git_remotes)\"" ; then
+    log_step "OVERWRITING __git_remotes function"
+    echo -e "\neval __orig_\"\$(declare -f __git_remotes)\"" >> "$startup_script"
+    echo -e "\n__git_remotes(){ __orig___git_remotes ; __url_completion ; }" >> "$startup_script"
+fi
+```
+
---
+++ lib/lib.sh
@@ -0,0 +1,102 @@
+# logs a step in "fancy"
+# format. $1 is the logging text
+# uses an environment variable
+# PREFIX_SPACES to change the prefix before
+# the "fancy" arrow. if PREFIX_SPACES
+# is null or unset, it will use a single space
+log_step() {
+    local logging_text="$1"
+    local arrow=" -> "
+    local prefix="${PREFIX_SPACES:-" "}"
+    echo "$prefix$arrow$logging_text"
+}
+
+# a helper function that checks if
+# __url_completion is already sourced
+# and if not, it sources it.
+# If it fails to source, it exits with an error.
+# It relies on an environment variable SCRIPTPATH being set
+# to the path of the install script
+source_url_completion_if_not_exist() {
+    # does __url_completion exist?
+    if [[ -z $(type -t "__url_completion") ]]; then
+        # nope: try to source it
+        log_step "SOURCING url_completion.sh"
+        source "$SCRIPTPATH/../../url_completion.sh" > /dev/null 2>&1
+        if [[ $? != "0" ]]; then
+            # something is horribly wrong :(
+            echo "Failed to source url_completion.sh file"
+            echo "is it missing? is it corrupted?"
+            echo "did you try to source this file instead of"
+            echo "running it? Please report this"
+            echo "error and the steps you took to reproduce it."
+            exit 1
+        fi
+    fi
+}
+
+
+# A helper function that checks if an environment
+# variable: STARTUP_SCRIPT_LOADED has been set already.
+# If not, it loads the startup script provided by
+# the first argument: $1
+load_startup_script_if_not_exist() {
+    # only load the script if it has not
+    # been loaded as an environment variable
+    if [[ -z $STARTUP_SCRIPT_LOADED ]]; then
+        log_step "LOADING STARTUP SCRIPT $1"
+        STARTUP_SCRIPT_LOADED="$(<$1)"
+    fi
+}
+
+
+# A helper function that checks if an environment variable:
+# STARTUP_SCRIPT_CONTAINS_URL_COMPLETION has been set already.
+# If not, it checks if the __url_completion function exists
+# in the STARTUP_SCRIPT_LOADED. If it fails to find __url_completion
+# in the STARTUP_SCRIPT, it writes it into the startup script
+# provided by $1.
+write_url_completion_to_file_if_not_exist() {
+    # does it already contain the url completion function?
+    if [[ -z $STARTUP_SCRIPT_CONTAINS_URL_COMPLETION ]]; then
+        # nope, env var not set, check if the function exists:
+        if ! startup_script_has "__url_completion" ; then
+            log_step "ADDING __url_completion function"
+            echo -e "\n$(declare -f __url_completion)" >> "$1"
+        fi
+        # set env var so future scripts
+        # dont bother checking it
+        STARTUP_SCRIPT_CONTAINS_URL_COMPLETION="true"
+    fi
+}
+
+# A helper function that writes the COMP_WORDBREAKS
+# colon removal line if it does not exist in the
+# STARTUP_SCRIPT_LOADED variable. It will write
+# to a file provided by $1
+write_colon_removal_to_file_if_not_exist() {
+    # does it already contain the wordbreaks colon removal?
+    if [[ -z $STARTUP_SCRIPT_CONTAINS_WORDBREAKS_REMOVAL ]]; then
+        # nope, env variable was not set, so lets check it now:
+        if ! startup_script_has "COMP_WORDBREAKS=\${COMP_WORDBREAKS//:}"; then
+            # no, it does not contain the wordbreaks line, so lets add it:
+            log_step "REMOVING ':' from COMP_WORDBREAKS"
+            echo -e "\n\nCOMP_WORDBREAKS=\${COMP_WORDBREAKS//:}" >> "$1"
+        fi
+        # set env variable so that future scripts run in the same
+        # subshell dont bother checking for it
+        STARTUP_SCRIPT_CONTAINS_WORDBREAKS_REMOVAL="true"
+    fi
+}
+
+# returns true
+# If STARTUP_SCRIPT_LOADED contains
+# the string given by $1, false otherwise.
+startup_script_has() {
+    if [[ $STARTUP_SCRIPT_LOADED == *"$1"* ]]; then
+        true
+    fi
+    false
+}
+
+__COMPLETION_LIB_LOADED="true"
