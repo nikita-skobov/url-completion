6884ce081ce630fbee060de3e2a047b8e3e7007a
nikita-skobov <nskobov.websites@gmail.com>
2020-05-17 12:56:43 -0500

    adds TODO

--- url_completion.sh
+++ url_completion.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# TODO:
+# make it ignore comments from the url_completion_file
+# make it ignore newlines
+
 # echo out a list of URLS
 # located in $__url_completion_file
 # which, if not defined, defaults to $HOME/.url_history
